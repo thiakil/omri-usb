thiakil/omri-usb

commit 0537c3027cd41f55642b895ed8aead3ce7cdd066

Introduce DabThread instead of direct std::thread. DabThread can be typedef'd to DabJoiningThread, but so far no benefit found.

omriusb/src/main/cpp/platformspecific/android/raontunerinput.cpp
@@ -27,6 +27,7 @@
 #include <set>
 
 #include <pthread.h>
+#include <time.h>
 #include <unistd.h>
 #include <syscall.h>
 #include <sys/endian.h>
@@ -318,17 +319,20 @@ void RaonTunerInput::stopScanCommand() {
 void RaonTunerInput::stopScanCommandThread() {
     std::lock_guard<std::recursive_mutex> lockGuard(m_classmutex);
     if (m_scanCommandThreadRunning) {
-        std::cout << LOG_TAG << "Stopping ScanCommand thread..." << std::endl;
         m_scanCommandThreadRunning = false;
-        if (m_scanCommandThread.joinable()) {
-            if (std::this_thread::get_id() != m_scanCommandThread.get_id()) {
+        if (m_scanCommandThread.get() == nullptr) {
+            return; // no thread, nothing to do
+        }
+        std::cout << LOG_TAG << "Stopping ScanCommand thread..." << std::endl;
+        if (m_scanCommandThread->joinable()) {
+            if (std::this_thread::get_id() != m_scanCommandThread->get_id()) {
                 std::cout << LOG_TAG << "Joining ScanCommand thread..." << std::endl;
-                m_scanCommandThread.join();
+                m_scanCommandThread->join();
                 std::cout << LOG_TAG << "Joining ScanCommand thread done" << std::endl;
             } else {
                 // cannot join myself
                 std::clog << LOG_TAG << "Can't join ScanCommand thread: it's me" << std::endl;
-                m_scanCommandThread.detach(); // forget about this thread
+                m_scanCommandThread->detach(); // forget about this thread
             }
         } else {
             std::clog << LOG_TAG << "ScanCommand thread not joinable" << std::endl;
@@ -343,7 +347,8 @@ void RaonTunerInput::startServiceScan() {
         stopScanCommandThread(); // in case it is still running
 
         m_scanCommandThreadRunning = true;
-        m_scanCommandThread = std::thread(&RaonTunerInput::processScanCommands, this);
+        m_scanCommandThread = std::unique_ptr<DabThread>(
+                new DabThread([this]() { processScanCommands(); }));
         m_scanCommandQueue.push(std::bind(&RaonTunerInput::startScanCommand, this));
     }
 }
@@ -1436,35 +1441,35 @@ void RaonTunerInput::scanningReadFic() {
 }
 
 void RaonTunerInput::rawRecordOpen(const std::shared_ptr<JDabService>& serviceLink) {
-    std::string labelstring;
-    std::stringstream serviceidstring, ensembleidstring;
-
     if (serviceLink != nullptr) {
         std::shared_ptr<DabService> dabService = serviceLink->getLinkDabService();
+
+        std::string labelString;
         if (dabService != nullptr) {
-            labelstring = dabService->getServiceLabel();
+            labelString = dabService->getServiceLabel();
         } else {
-            labelstring = "labelunknown";
+            labelString = "labelunknown";
         }
         // "_" is used to find the infos from the filename, get rid of it in the label, but replace with " "
-        labelstring = std::regex_replace(labelstring, std::regex("_"), " ");
-        serviceidstring << std::hex << serviceLink->getServiceId();
-        ensembleidstring << std::hex << serviceLink->getEnsembleId();
-        const std::string filenameAfterDateWithoutSuffix =
-                labelstring
-                + "_" + ensembleidstring.str()
-                + "_" + serviceidstring.str();
+        labelString = std::regex_replace(labelString, std::regex("_"), " ");
+
+        char serviceIdCString[11];
+        char ensembleIdCString[11];
+        snprintf(serviceIdCString, sizeof(serviceIdCString)-1, "%x", serviceLink->getServiceId());
+        serviceIdCString[sizeof(serviceIdCString)-1] = '\0';
+        snprintf(ensembleIdCString, sizeof(ensembleIdCString)-1, "%x", serviceLink->getEnsembleId());
+        ensembleIdCString[sizeof(ensembleIdCString)-1] = '\0';
+        std::string filenameAfterDateWithoutSuffix = labelString;
+        filenameAfterDateWithoutSuffix.append("_").append(serviceIdCString);
+        filenameAfterDateWithoutSuffix.append("_").append(ensembleIdCString);
 
         __rawRecordOpen(filenameAfterDateWithoutSuffix);
     }
 }
 
 void RaonTunerInput::rawRecordOpen(const int freqMhz) {
-    std::stringstream freqMhzStrStream;
-    freqMhzStrStream << std::dec << freqMhz;
-
-    const std::string filenameAfterDateWithoutSuffix =
-            "scan_" + freqMhzStrStream.str();
+    std::string filenameAfterDateWithoutSuffix = "scan_";
+    filenameAfterDateWithoutSuffix.append(std::to_string(freqMhz));
 
     __rawRecordOpen(filenameAfterDateWithoutSuffix);
 }
@@ -1483,13 +1488,27 @@ void RaonTunerInput::__rawRecordOpen(const std::string& filenameAfterDateWithout
     // open new one
     auto now = std::chrono::system_clock::now();
     auto in_time_t = std::chrono::system_clock::to_time_t(now);
-    std::stringstream timestring;
-    timestring << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d_%H-%M-%S");
 
-    const std::string filename =
-            m_recordPath + "/" + "dab_" + timestring.str() + "_" +
-            filenameAfterDateWithoutSuffix + ".raw";
+    char timeCstring[4+1+2+1+2+1+2+1+2+1+2 + 2]; // +2 because strftime will put also '\0' at end
+    const tm* pTm = std::localtime(&in_time_t);
+    if (0 == std::strftime(timeCstring, sizeof(timeCstring)-1, "%Y-%m-%d_%H-%M-%S", pTm)) {
+        std::clog << LOG_TAG << "rawRecordOpen strftime failed" << std::endl;
+        return;
+    }
+    timeCstring[sizeof(timeCstring)-1] = '\0';
 
+    std::cout << LOG_TAG << "rawRecordOpen construct filename from " << m_recordPath << " and "
+              << timeCstring
+              << " and " << filenameAfterDateWithoutSuffix << std::endl;
+
+    std::string filename = m_recordPath;
+    filename.append("/dab_")
+        .append(timeCstring)
+        .append("_")
+        .append(filenameAfterDateWithoutSuffix)
+        .append(".raw");
+
+    std::cout << LOG_TAG << "rawRecordOpen... " << filename << std::endl;
     m_outFileStream.open(filename, std::ios::out | std::ios::binary);
 
     if (m_outFileStream.is_open()) {
@@ -1597,7 +1616,8 @@ void RaonTunerInput::startReadFicThread() {
     if(!m_readFicThreadRunning) {
         std::cout << LOG_TAG << "Starting FIC thread..." << std::endl;
         m_readFicThreadRunning = true;
-        m_readFicThread = std::thread(&RaonTunerInput::threadedScanningFicRead, this);
+        m_readFicThread = std::unique_ptr<DabThread>(
+                new DabThread([this]() { threadedScanningFicRead(); } ));
     } else {
         std::clog << LOG_TAG << "FIC thread already running" << std::endl;
     }
@@ -1606,17 +1626,20 @@ void RaonTunerInput::startReadFicThread() {
 void RaonTunerInput::stopReadFicThread() {
     std::lock_guard<std::recursive_mutex> lockGuard(m_classmutex);
     if(m_readFicThreadRunning) {
-        std::cout << LOG_TAG << "Stopping FIC thread..." << std::endl;
         m_readFicThreadRunning = false;
-        if(m_readFicThread.joinable()) {
-            if (std::this_thread::get_id() != m_readFicThread.get_id()) {
+        if (m_readFicThread.get() == nullptr) {
+            return; // no thread, nothing to do
+        }
+        std::cout << LOG_TAG << "Stopping FIC thread..." << std::endl;
+        if(m_readFicThread->joinable()) {
+            if (std::this_thread::get_id() != m_readFicThread->get_id()) {
                 std::cout << LOG_TAG << "Joining FIC thread..." << std::endl;
-                m_readFicThread.join();
+                m_readFicThread->join();
                 std::cout << LOG_TAG << "Joining FIC thread done" << std::endl;
             } else {
                 // cannot join myself
                 std::clog << LOG_TAG << "Can't join FIC thread: it's me" << std::endl;
-                m_readFicThread.detach(); // forget about this thread
+                m_readFicThread->detach(); // forget about this thread
             }
         } else {
             std::clog << LOG_TAG << "FIC thread not joinable" << std::endl;
@@ -1628,13 +1651,19 @@ void RaonTunerInput::startReadDataThread() {
     if(!m_commandThreadRunning) {
         std::cout << LOG_TAG << "Starting Data thread..." << std::endl;
         m_commandThreadRunning = true;
-        if (std::this_thread::get_id() != m_commandThread.get_id()) {
-            m_commandThread = std::thread(&RaonTunerInput::commandProcessing, this);
-        } else {
-            // running on the thread that I should start ?!?
-            std::cout << LOG_TAG << "Continue Data thread with NOP" << std::endl;
+        if (m_commandThread.get() == nullptr) {
+            m_commandThread = std::unique_ptr<DabThread>(
+                    new DabThread([this]() { commandProcessing(); }));
+        } else if (m_commandThread->joinable()) { // thread seems to be running
+            std::clog << LOG_TAG << "Continue Data thread with NOP" << std::endl;
             // trigger myself with a NOP
             m_commandQueue.push(std::bind(&RaonTunerInput::nop, this));
+        } else {
+            std::clog << LOG_TAG << "Re-Create Data thread" << std::endl;
+            // create a new thread, other seems already dead
+            m_commandThread.release();
+            m_commandThread = std::unique_ptr<DabThread>(
+                    new DabThread([this]() { commandProcessing(); }));
         }
     } else {
         std::clog << LOG_TAG << "Starting Data thread: already running" << std::endl;
@@ -1645,12 +1674,15 @@ void RaonTunerInput::stopReadDataThread() {
     std::lock_guard<std::recursive_mutex> lockGuard(m_classmutex);
 
     if(m_commandThreadRunning) {
-        std::cout << LOG_TAG << "Stopping Data thread..." << std::endl;
         m_commandThreadRunning = false;
-        if(m_commandThread.joinable()) {
-            if (std::this_thread::get_id() != m_commandThread.get_id()) {
+        if (m_commandThread.get() == nullptr) {
+            return; // no thread, nothing to do
+        }
+        std::cout << LOG_TAG << "Stopping Data thread..." << std::endl;
+        if(m_commandThread->joinable()) {
+            if (std::this_thread::get_id() != m_commandThread->get_id()) {
                 std::cout << LOG_TAG << "Join Data thread..." << std::endl;
-                m_commandThread.join();
+                m_commandThread->join();
                 std::cout << LOG_TAG << "Join Data thread done" << std::endl;
             } else {
                 // cannot join myself, trigger myself with a NOP

omriusb/CMakeLists.txt
@@ -69,6 +69,7 @@ add_library( # Sets the name of the library.
              src/main/cpp/dabensemble.cpp
              src/main/cpp/dabservicecomponentmscstream.cpp
              src/main/cpp/ficparser.cpp
+             src/main/cpp/dabthread.cpp
              src/main/cpp/platformspecific/android/raontunerinput.cpp
              src/main/cpp/platformspecific/android/jusbdevice.cpp
              src/main/cpp/platformspecific/android/jtunerusbdevice.cpp

omriusb/src/main/cpp/dabensemble.cpp
@@ -20,10 +20,10 @@
 
 #include <functional>
 #include <iostream>
+#include <memory>
 
 #include "dabensemble.h"
 #include "dabservice.h"
-#include "timer.h"
 
 DabEnsemble::DabEnsemble() {
     std::cout << m_logTag << " Constructing" << std::endl;
@@ -49,10 +49,12 @@ void DabEnsemble::reset() {
     if (m_ficPtr != nullptr) {
         m_ficPtr->stop();
         m_ficPtr->reset();
-        registerCbs();
-        if (!m_ficPtr->isStarted()) {
-            m_ficPtr->start();
-        }
+        m_ficPtr.release();
+    }
+    m_ficPtr = std::unique_ptr<FicParser>(new FicParser);
+    registerCbs();
+    if (!m_ficPtr->isStarted()) {
+        m_ficPtr->start();
     }
 
     // reset start time

omriusb/src/main/cpp/dabmpegservicecomponentdecoder.cpp
@@ -32,14 +32,17 @@ DabMpegServiceComponentDecoder::DabMpegServiceComponentDecoder() {
     //std::cout << m_logTag << "Creating" << std::endl;
 
     m_processThreadRunning = true;
-    m_processThread = std::thread(&DabMpegServiceComponentDecoder::processData, this);
+    m_processThread = std::unique_ptr<DabThread>(
+            new DabThread( [this] () { processData(); }));
 }
 
 DabMpegServiceComponentDecoder::~DabMpegServiceComponentDecoder() {
     //std::cout << m_logTag << "Destroying" << std::endl;
 
     m_processThreadRunning = false;
-    m_processThread.join();
+    if (m_processThread->joinable()) {
+        m_processThread->join();
+    }
     m_conQueue.clear();
 }
 

omriusb/src/main/cpp/dabmpegservicecomponentdecoder.h
@@ -21,12 +21,12 @@
 #ifndef DABMPEGSERVICECOMPONENTDECODER_H
 #define DABMPEGSERVICECOMPONENTDECODER_H
 
-#include <thread>
 #include <atomic>
-
-#include "dabservicecomponentdecoder.h"
+#include <thread>
 
 #include "concurrent_queue.h"
+#include "dabservicecomponentdecoder.h"
+#include "dabthread.h"
 
 class DabMpegServiceComponentDecoder : public DabServiceComponentDecoder {
 
@@ -48,7 +48,7 @@ private:
 
     ConcurrentQueue <std::vector<uint8_t>> m_conQueue;
     std::atomic<bool> m_processThreadRunning{false};
-    std::thread m_processThread;
+    std::unique_ptr<DabThread> m_processThread;
 
     CallbackDispatcher<std::function<void (const std::vector<uint8_t>&)>> m_padDataDispatcher;
     CallbackDispatcher<AUDIO_COMPONENT_DATA_CALLBACK> m_audioDataDispatcher;

omriusb/src/main/cpp/dabplusservicecomponentdecoder.cpp
@@ -29,15 +29,16 @@ extern "C" {
 
 DabPlusServiceComponentDecoder::DabPlusServiceComponentDecoder() {
     m_processThreadRunning = true;
-    m_processThread = std::thread(&DabPlusServiceComponentDecoder::processData, this);
+    m_processThread = std::unique_ptr<DabThread>(
+            new DabThread([this]() { processData(); } ));
 }
 
 DabPlusServiceComponentDecoder::~DabPlusServiceComponentDecoder() {
     //std::cout << m_logTag << " Deconstructing" << std::endl;
 
     m_processThreadRunning = false;
-    if(m_processThread.joinable()) {
-        m_processThread.join();
+    if(m_processThread->joinable()) {
+        m_processThread->join();
     }
     //m_conQueue.clear();
 }

omriusb/src/main/cpp/dabplusservicecomponentdecoder.h
@@ -21,18 +21,17 @@
 #ifndef DABPLUSSERVICECOMPONENTDECODER_H
 #define DABPLUSSERVICECOMPONENTDECODER_H
 
-#include <vector>
+#include <atomic>
+#include <iostream>
 #include <string>
 #include <thread>
-#include <atomic>
+#include <vector>
 
-#include <iostream>
-
-#include "dabservicecomponentdecoder.h"
 #include "concurrent_queue.h"
-
-#include "registered_tables.h"
+#include "dabservicecomponentdecoder.h"
+#include "dabthread.h"
 #include "global_definitions.h"
+#include "registered_tables.h"
 
 class DabPlusServiceComponentDecoder : public DabServiceComponentDecoder {
 
@@ -93,7 +92,7 @@ private:
     CallbackDispatcher<AUDIO_COMPONENT_DATA_CALLBACK> m_audioDataDispatcher;
 
     std::atomic<bool> m_processThreadRunning{false};
-    std::thread m_processThread;
+    std::unique_ptr<DabThread>  m_processThread;
 
     std::vector<uint8_t> m_unsyncDataBuffer;
     bool m_unsyncSync{false};

omriusb/src/main/cpp/dabthread.cpp
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2023 realzoulou
+ *
+ * Author:
+ *  realzoulou
+ *
+ * This file is a part of IRT DAB library.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#include <string>
+
+#include "dabthread.h"
+
+const std::string DabJoiningThread::LOG_TAG{"[DabJoiningThread] "};
+
+DabJoiningThread::~DabJoiningThread() {
+    const std::string tidStr = std::to_string(mNativeThreadId);
+    std::string str = LOG_TAG;
+    if (joinable()) {
+        str.append("~Joining thread ").append(tidStr);
+        std::cout << str << std::endl;
+        join();
+        str = LOG_TAG;
+        str.append("~Joined thread ").append(tidStr);
+        std::cout << str << std::endl;
+    } else {
+        str.append("Not ~Joinable thread ").append(tidStr);
+        std::cout << str << std::endl;
+    }
+    Base::~Base();
+}
+
+void DabJoiningThread::join() {
+    const long joinerThreadId = getCurrentThreadNativeId();
+    const std::string joinStr = std::to_string(joinerThreadId);
+    const std::string tidStr = std::to_string(mNativeThreadId);
+    std::string str = LOG_TAG;
+    str.append("join thread ").append(tidStr).append(" from thread ").append(joinStr);
+    std::cout << str << std::endl;
+    Base::join();
+    str = LOG_TAG;
+    str.append("joined thread ").append(tidStr).append(" from thread ").append(joinStr);
+    std::cout << str << std::endl;
+}
+
+void DabJoiningThread::threadFuncPre() {
+    // Don't access mNativeThreadId !
+    const long myId = getCurrentThreadNativeId();
+    mNativeThreadId = myId;
+    std::string str = LOG_TAG;
+    str.append("Starting thread ").append(std::to_string(myId));
+    std::cout << str << std::endl;
+}
+void DabJoiningThread::threadFuncPost() {
+    // Don't access mNativeThreadId !
+    const long myId = getCurrentThreadNativeId();
+    std::string str = LOG_TAG;
+    str.append("Finished thread ").append(std::to_string(myId));
+    std::cout << str << std::endl;
+}
+void DabJoiningThread::threadFuncException() {
+    // Don't access mNativeThreadId !
+    const long myId = getCurrentThreadNativeId();
+    std::string str = LOG_TAG;
+    str.append("Exception thread ").append(std::to_string(myId));
+    std::cout << str << std::endl;
+}

commit e39b2c141139379df2cfe9c8a654ed0839539787

Fix OMRI raw recording filename created with EId/SId swapped. Regression introduced with [0537c30]

omriusb/src/main/cpp/platformspecific/android/raontunerinput.cpp
@@ -1460,8 +1460,8 @@ void RaonTunerInput::rawRecordOpen(const std::shared_ptr<JDabService>& serviceLi
         snprintf(ensembleIdCString, sizeof(ensembleIdCString)-1, "%x", serviceLink->getEnsembleId());
         ensembleIdCString[sizeof(ensembleIdCString)-1] = '\0';
         std::string filenameAfterDateWithoutSuffix = labelString;
-        filenameAfterDateWithoutSuffix.append("_").append(serviceIdCString);
         filenameAfterDateWithoutSuffix.append("_").append(ensembleIdCString);
+        filenameAfterDateWithoutSuffix.append("_").append(serviceIdCString);
 
         __rawRecordOpen(filenameAfterDateWithoutSuffix);
     }