13f4eb495d2bf862d55cbd0f136519fe7c30af08
Thiakil <thiakil@users.noreply.github.com>
2026-07-09 20:45:18 +0800

    hardening CallbackDispatcher and add clear() method

--- omri-usb-native/src/main/cpp/callbackhandle.h
+++ omri-usb-native/src/main/cpp/callbackhandle.h
@@ -36,13 +36,6 @@ template <typename C> class CallbackDispatcher final {
 public:
     ~CallbackDispatcher() { }
 
-    //TODO protect list against data races??
-    std::shared_ptr<C> add(C&& callback) {
-        auto shared = std::make_shared<C>(callback);
-        this->callbacks.push_back(shared);
-        return shared;
-    }
-
     std::shared_ptr<C> add(C& callback) {
         auto shared = std::make_shared<C>(callback);
         this->callbacks.push_back(shared);
@@ -50,36 +43,46 @@ public:
     }
 
     bool hasCallbacks() {
+        // Remove all callbacks that are gone, only if we are not dispatching.
+        if (this->concurrent_dispatcher_count == 0) {
+            this->callbacks.erase(std::remove_if(this->callbacks.begin(), this->callbacks.end(),
+                                                 [](std::weak_ptr<C> callback) {
+                                                     return callback.expired();
+                                                 }), this->callbacks.end());
+        }
         return !callbacks.empty();
+    }
+
+    void clear() {
+        this->callbacks.clear();
     }
 
     template <typename ...A>
     void invoke(A && ... args) {
+        // Remove all callbacks that are gone
+        hasCallbacks();
+
         this->concurrent_dispatcher_count++;
 
-        try {
-            size_t current = 0;
-            while (current < this->callbacks.size()) {
+        size_t current = 0;
+        while (current < this->callbacks.size()) {
+            try {
                 if (auto callback = this->callbacks[current++].lock()) {
-                    (*callback)(std::forward<A>(args)...);
+                    if (callback != nullptr &&
+                        ((static_cast<std::shared_ptr<C>>(callback)).get() != nullptr)) {
+                        (*callback)(std::forward<A>(args)...);
+                    }
                 }
-            }
-            this->concurrent_dispatcher_count--;
-        } catch (...) {
-            this->concurrent_dispatcher_count--;
-            throw;
+            } catch (...) {}
         }
 
-        // Remove all callbacks that are gone, only if we are not dispatching.
-        if (0 == this->concurrent_dispatcher_count) {
-            this->callbacks.erase(std::remove_if(this->callbacks.begin(), this->callbacks.end(), [] (std::weak_ptr<C> callback) { return callback.expired(); }), this->callbacks.end());
-        }
+        this->concurrent_dispatcher_count--;
     }
 
 private:
     //maybe use a list for constant iterators
     std::vector<std::weak_ptr<C>> callbacks;
-    int32_t concurrent_dispatcher_count = 0;
+    std::atomic_int32_t concurrent_dispatcher_count{0};
 };
 
 #endif // CALLBACKHANDLE
