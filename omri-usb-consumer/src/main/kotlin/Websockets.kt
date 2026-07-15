51c9e25ae2859e2d9e79e6d7fac0d1d94abca853
Thiakil <thiakil@users.noreply.github.com>
2026-07-15 19:45:30 +0800

    kx.ser websockets

--- omri-usb-consumer/src/main/kotlin/Websockets.kt
+++ omri-usb-consumer/src/main/kotlin/Websockets.kt
@@ -1,16 +1,17 @@
 package com.thiakil
 
-import io.ktor.serialization.gson.GsonWebsocketContentConverter
+import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.websocket.WebSockets
 import io.ktor.server.websocket.pingPeriod
 import io.ktor.server.websocket.timeout
+import kotlinx.serialization.json.Json
 import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureWebsockets() {
     install(WebSockets) {
-        contentConverter = GsonWebsocketContentConverter()
+        contentConverter = KotlinxWebsocketSerializationConverter(Json)
         pingPeriod = 15.seconds
         timeout = 15.seconds
         maxFrameSize = Long.MAX_VALUE
