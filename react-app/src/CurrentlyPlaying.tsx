81be03fb258fbbc18cb7200c7bcf342f8f4c60c3
Thiakil <thiakil@users.noreply.github.com>
2026-07-30 21:02:21 +0800

    make slideshow holder the right aspect ratio for dab slideshows

--- react-app/src/CurrentlyPlaying.tsx
+++ react-app/src/CurrentlyPlaying.tsx
@@ -16,25 +16,26 @@ function CurrentlyPlaying({service, currentText = "", onStop, currentSlideshow}:
         </>
     )
   }
-  return (<div className="w-11/12 flex gap-4">
-    <div className="h-46vh">
-    <div className={
-        "size-46vh shrink-0 grow-0 rounded-lg overflow-clip relative " +
-          (hasService ? ' bg-primary-container text-on-primary-container' : ' bg-surface-container-low text-surface-container-highest') +
-          (currentSlideshow ? '' : ' h-46vh')
-      }>
+  return (
+      <div className="w-11/12 flex gap-4">
+        <div className={
+            "slideshow-holder shrink-0 grow-0 rounded-lg overflow-clip relative " +
+            (hasService ? ' bg-primary-container text-on-primary-container' : ' bg-surface-container-low text-surface-container-highest')
+        }>
           {currentSlideshow ?
-              (<img src={currentSlideshow} alt="slideshow" className="object-cover w-full" />)
+              (<img src={currentSlideshow} alt="slideshow" className="object-cover w-full"/>)
               :
-              <div className="absolute inset-0 flex items-center justify-center"><mdui-icon name="cell_tower"></mdui-icon></div>
+              <div className="absolute inset-0 flex items-center justify-center">
+                <mdui-icon name="cell_tower"></mdui-icon>
+              </div>
           }
-      </div>
-    </div>
-      <div className="grow">
-        <h1 className={"font-bold" + (hasService ? '' : ' text-gray-300')}>{service?.serviceLabel || "Inactive"}</h1>
-        {nowPlaying}
-      </div>
-    </div>);
+        </div>
+
+        <div className="grow">
+          <h1 className={"font-bold" + (hasService ? '' : ' text-gray-300')}>{service?.serviceLabel || "Inactive"}</h1>
+          {nowPlaying}
+        </div>
+      </div>);
 };
 
 export default CurrentlyPlaying;
